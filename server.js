e204d8d2ab0234af7a4086e8d193c846e105983d
Priyanka Gholap <your-github-priyankagholap182@gmail.com>
2026-01-08 23:48:07 +0530

    Backend ready

--- server.js
+++ server.js
@@ -341,7 +341,8 @@ app.post("/prescription/:consultationId", async (req, res) => {
 
 
 /* ================= SERVER ================= */
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
-  console.log("Server running on 5000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
